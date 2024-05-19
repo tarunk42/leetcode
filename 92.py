ed8f78f9728460df658e8217f1da2e2019421b0e
tarun kashyap <tarunsinghkashyap@gmail.com>
2024-05-19 22:12:31 +0100

    92_soln

--- 92.py
+++ 92.py
@@ -5,5 +5,42 @@
 
 from linkedlist.linkedlist import LinkedList
 
+class Solution:
+    def reverseBetween(self, head, left, right):
+        if not head or left == right:
+            return head
+        
+        dummy = LinkedList.Node()
+        dummy.next = head
+        previous = dummy
 
+        for _ in range(left):
+            previous = previous.next
+
+        current = previous.next
+        next_node = None
+
+        for i in range(right-left):
+            next_node = current.next
+            current.next = next_node.next
+            next_node.next = previous.next
+            previous.next = next_node
+        
+        return dummy.next
+    
+
+ll1 = LinkedList.linkedList()
+for i in range(1,7):
+    ll1.add(i)
+print(ll1)
+
+print(ll1)
+
+soln = Solution()
+# new_head = soln.reverseList(ll1.head)
+new_head = soln.reverseBetween(ll1.head, 1, 4)
+
+ll1.head = new_head
+
+print(ll1)
 
