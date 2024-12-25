f54d55d25c2ccf47a17079bf583b2ee3a19f6354
tarun kashyap <tarunsinghkashyap@gmail.com>
2024-12-25 15:14:14 +0000

    190

--- blind75/190.py
+++ blind75/190.py
@@ -1,2 +1,15 @@
 # Leetcode 190. Reverse Bits
 
+# Reverse bits of a given 32 bits unsigned integer.
+
+# brute force (bit manipulation)
+def reverseBits(n: int) -> int:
+    res = 0
+    for i in range(32):
+        res = (res << 1) + (n & 1)
+        n >>= 1
+    return res
+
+# example
+n = 43261596
+print(reverseBits(n)) # 964176192
